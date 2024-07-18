692b08cfe5e61638927f07ccbea308eaaf878ad0
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-07-18 19:30:17 +0900

    전체 레이아웃

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener('scroll', function() {
+  const section2 = document.getElementById('section2');
+  const section2Top = section2.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  
+});
